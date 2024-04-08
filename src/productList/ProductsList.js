1600e748ae9718fb9ee4e61fc0fb0fad44de0e3c
Ringo2901 <nekitale@yandex.ru>
2024-04-08 21:20:57 +0300

    For deploy

---
+++ src/productList/ProductsList.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ProductsList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('https://lookmarket-8b7afe9ac5cf.herokuapp.com/api/web/product');
+                const { status, code, message, payload } = response.data;
+                if (status === 'OK') {
+                    setProducts(payload);
+                } else {
+                    console.error('Error fetching products:', message, code);
+                }
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Список товаров</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product.imageId}>
+                        <img src={`data:image;base64,${product.image}`} alt={product.title} />
+                        <p>{product.title}</p>
+                        <p>{product.price}</p>
+                        <p>{product.userId}</p>
+                        <p>{product.userLogin}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ProductsList;
